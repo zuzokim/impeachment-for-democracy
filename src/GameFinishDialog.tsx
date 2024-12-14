991dd872b12dea94cc4a886b1679c452d2b9a0ba
zuzokim <zuzokim@gmail.com>
2024-12-14 14:02:18 +0900

    모바일 다이얼로그 반응형 대응

--- src/GameFinishDialog.tsx
+++ src/GameFinishDialog.tsx
@@ -15,7 +15,7 @@ function GameFinishDialog({ open, onClose, score = 0 }: GameFinishDialogProps) {
           <div className="game-finish-title">게임 종료</div>
           <div className="game-finish-body">
             <div className="score">{score}점</div>
-            <div className="impeachment">윤석열 탄핵!</div>
+            <div className="impeachment">{`윤석열\n탄핵!`}</div>
           </div>
           <div className="game-finish-action">
             <button onClick={onClose}>다시 도전하기</button>
